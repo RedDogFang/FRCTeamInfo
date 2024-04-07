905c00f2ca8a338c5fe56da2e1ac03f3fc113e54
ryanK <ryan@tholos.app>
2024-04-07 11:58:31 -0700

    htmx and tailwind added

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from .views import example_endpoint, index
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', index, name='index'),
+    path('example-endpoint/', example_endpoint, name='example-endpoint'),
+
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def example_endpoint(request):
+    # This is the content that will be inserted into the HTML page.
+    content = "<div>This content was loaded with HTMX!</div>"
+    return HttpResponse(content)
+
 
 # Create your views here.
 def index(request): 
